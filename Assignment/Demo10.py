52b2694f71e2cf152ef81e0b97aeb44147a3b6cc
kanak Kushwah <kanakkushwah17@gmail.com>
2026-08-29 21:44:56 +0530

    Delete Assignment/Demo10.py

--- Assignment/Demo10.py
+++
@@ -1,9 +0,0 @@
-"""import mypackages
-print("main file")
-mypackages.calculation1()
-mypackages.calculation2()"""
-
-from mypackages import logic1,logic2
-print("main file")
-logic1.calculation1()
-logic2.calculation2()
